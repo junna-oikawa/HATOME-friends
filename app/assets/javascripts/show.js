8c1a2825da899bab8f0cd3541a67987b59c903ea
junna-oikawa <jnn.y326@gmail.com>
2021-11-15 06:49:26 +0000

    showページに飛ばす

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -2,6 +2,6 @@ var jsonLoad;
 
 window.onload = function () {
   jsonLoad = document.getElementById('data').value
-  alert(jsonLoad);
+  // alert(jsonLoad);
   var stage = Konva.Node.create(jsonLoad, 'container');
 }
--- app/assets/javascripts/zindex.js
+++ app/assets/javascripts/zindex.js
@@ -119,38 +119,15 @@ document.getElementById('save').addEventListener(
     form.method = "post";
     form.action = "/practices";
 
-    // for (const key in params) {
-    //   if (params.hasOwnProperty(key)) {
     const hiddenField = document.createElement('input');
     hiddenField.type = 'hidden';
     hiddenField.name = "json_data";
     hiddenField.value = json;
 
     form.appendChild(hiddenField);
-    //   }
-    // }
 
     document.body.appendChild(form);
     form.submit();
-
-    // var send_data = new XMLHttpRequest();
-    // send_data.open('POST', '/practices', false);
-    // send_data.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-    // send_data.send("data=" + json);
-
-
-    // var form = document.createElement('form');
-    // document.body.appendChild(form);
-    // form.method = 'post';
-    // form.action = '/practices';
-    // var input = document.createElement('input');
-    // input.type = 'hidden';
-    // input.name = name;
-    // input.value = json;
-    // form.appendChild(input);
-    // console.log(form);
-
-    // form.submit();
   },
   false
 );
